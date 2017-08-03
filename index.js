73d4403cc54b7a76b222ce6b34dd0b09b69b5e9d
xenotime-india <sandeepkhoj@gmail.com>
2017-08-03 12:23:00 +0530

    fix

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 /**
  * Created by sandeepkumar on 03/08/17.
  */
-const express = require('express');
-const bodyParser = require('body-parser');
-const config = require('config');
-const MessageValidator = require('sns-validator');
+import express from 'express';
+import config from 'config';
+import MessageValidator from 'sns-validator';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const validator = new MessageValidator();
 const app = express();
+// Helmet helps you secure your Express apps by setting various HTTP headers
+// https://github.com/helmetjs/helmet
+app.use(helmet());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// Enable CORS with various options
+// https://github.com/expressjs/cors
+app.use(cors());
+
+// Request logger
+// https://github.com/expressjs/morgan
+if (!config.test) {
+  app.use(morgan('dev'));
+}
+
+// Parse incoming request bodies
+// https://github.com/expressjs/body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/validate', function (req, res) {
   validator.validate(req.body, function (err, message) {
